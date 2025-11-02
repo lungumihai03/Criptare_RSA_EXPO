d2222cfd2606ea8afcc29126b8f2f2200e85aedf
Mihai Lungu <lungu.mihai03@gmail.com>
2025-11-02 22:00:56 +0200

    Add project files.

---
+++ Criptare_RSA_EXPO/Form1.Designer.cs
@@ -0,0 +1,270 @@
+﻿namespace Criptare_RSA_EXPO
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.txtPlain = new System.Windows.Forms.RichTextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txtEncrypted = new System.Windows.Forms.RichTextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.txtDecrypted = new System.Windows.Forms.RichTextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(602, 460);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(127, 33);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Cript";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.btnEncrypt_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(735, 460);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(127, 33);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Decript";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.btnDecrypt_Click);
+            // 
+            // txtPlain
+            // 
+            this.txtPlain.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPlain.Location = new System.Drawing.Point(12, 109);
+            this.txtPlain.Name = "txtPlain";
+            this.txtPlain.Size = new System.Drawing.Size(850, 94);
+            this.txtPlain.TabIndex = 25;
+            this.txtPlain.Text = "";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(12, 86);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(75, 20);
+            this.label9.TabIndex = 26;
+            this.label9.Text = "Mesajul:";
+            // 
+            // txtEncrypted
+            // 
+            this.txtEncrypted.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEncrypted.Location = new System.Drawing.Point(12, 230);
+            this.txtEncrypted.Name = "txtEncrypted";
+            this.txtEncrypted.Size = new System.Drawing.Size(850, 94);
+            this.txtEncrypted.TabIndex = 27;
+            this.txtEncrypted.Text = "";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(12, 207);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(134, 20);
+            this.label10.TabIndex = 28;
+            this.label10.Text = "Mesajul Criptat:";
+            // 
+            // txtDecrypted
+            // 
+            this.txtDecrypted.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDecrypted.Location = new System.Drawing.Point(12, 360);
+            this.txtDecrypted.Name = "txtDecrypted";
+            this.txtDecrypted.Size = new System.Drawing.Size(850, 94);
+            this.txtDecrypted.TabIndex = 29;
+            this.txtDecrypted.Text = "";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(12, 337);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(154, 20);
+            this.label11.TabIndex = 30;
+            this.label11.Text = "Mesajul Decriptat:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(450, 460);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(127, 33);
+            this.button1.TabIndex = 31;
+            this.button1.Text = "Gen Key";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(28, 34);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 32;
+            this.button4.Text = "afiseaza P";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.btnP_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(176, 34);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 33;
+            this.button5.Text = "afiseaza Q";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.btnQ_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(305, 34);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 34;
+            this.button6.Text = "Afiseaza N";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.btnN_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(414, 34);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(105, 23);
+            this.button7.TabIndex = 35;
+            this.button7.Text = "afiseaza Fi(N)";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.btnPhi_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(573, 34);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(75, 23);
+            this.button8.TabIndex = 36;
+            this.button8.Text = "Afiseaza E";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.btnE_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(712, 34);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(75, 23);
+            this.button9.TabIndex = 37;
+            this.button9.Text = "Afiseaza D";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.btnD_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(305, 468);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            500,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 38;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(874, 500);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.txtDecrypted);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.txtEncrypted);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.txtPlain);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.HelpButton = true;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Criptarea RSA prin exponentiere";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.RichTextBox txtPlain;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.RichTextBox txtEncrypted;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.RichTextBox txtDecrypted;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+    }
+}
+
---
+++ Criptare_RSA_EXPO/Form1.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Criptare_RSA_EXPO
+{
+    public partial class Form1 : Form
+    {
+        BigInteger p, q, n, phi, e, d;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // ===================== GENERARE CHEI =====================
+        private void btnGenerate_Click(object sender, EventArgs ev)
+        {
+            try
+            {
+                int digits = (int)numericUpDown1.Value;
+                if (digits < 10) digits = 10;
+                if (digits > 500) digits = 500;
+
+                p = GenereazaPrim(digits);
+                q = GenereazaPrim(digits);
+                n = p * q;
+                phi = (p - 1) * (q - 1);
+                e = GenereazaE(phi);
+                d = InverseMod(e, phi);
+
+               
+
+                MessageBox.Show($"Chei RSA ({digits} cifre) generate cu succes!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Eroare: " + ex.Message);
+            }
+        }
+
+        // ===================== GENERARE NUMĂR PRIM =====================
+        private BigInteger GenereazaPrim(int digits)
+        {
+            Random rnd = new Random();
+            BigInteger numar;
+
+            do
+            {
+                string s = "1";
+                for (int i = 1; i < digits - 1; i++)
+                    s += rnd.Next(0, 10).ToString();
+                s += rnd.Next(1, 9).ToString();
+                numar = BigInteger.Parse(s);
+            } while (!MillerRabin(numar, 15));
+
+            return numar;
+        }
+
+        // ===================== TEST MILLER–RABIN =====================
+        private bool MillerRabin(BigInteger n, int k)
+        {
+            if (n < 2) return false;
+            if (n == 2 || n == 3) return true;
+            if (n % 2 == 0) return false;
+
+            BigInteger d = n - 1;
+            int s = 0;
+            while (d % 2 == 0)
+            {
+                d /= 2;
+                s++;
+            }
+
+            Random rnd = new Random();
+            for (int i = 0; i < k; i++)
+            {
+                BigInteger a = RandomBigInteger(2, n - 2, rnd);
+                BigInteger x = BigInteger.ModPow(a, d, n);
+                if (x == 1 || x == n - 1)
+                    continue;
+
+                bool cont = false;
+                for (int r = 1; r < s; r++)
+                {
+                    x = BigInteger.ModPow(x, 2, n);
+                    if (x == n - 1)
+                    {
+                        cont = true;
+                        break;
+                    }
+                }
+                if (!cont) return false;
+            }
+            return true;
+        }
+
+        private BigInteger RandomBigInteger(BigInteger min, BigInteger max, Random rnd)
+        {
+            byte[] bytes = max.ToByteArray();
+            BigInteger r;
+            do
+            {
+                rnd.NextBytes(bytes);
+                bytes[bytes.Length - 1] &= 0x7F;
+                r = new BigInteger(bytes);
+            } while (r < min || r >= max);
+            return r;
+        }
+
+        // ===================== GENERARE E =====================
+        private BigInteger GenereazaE(BigInteger phi)
+        {
+            Random rnd = new Random();
+            BigInteger e;
+            do
+            {
+                e = RandomBigInteger(3, phi - 1, rnd);
+            } while (BigInteger.GreatestCommonDivisor(e, phi) != 1);
+            return e;
+        }
+
+        // ===================== INVERSE MODULAR =====================
+        private BigInteger InverseMod(BigInteger a, BigInteger m)
+        {
+            BigInteger m0 = m, y = 0, x = 1;
+            if (m == 1) return 0;
+
+            while (a > 1)
+            {
+                BigInteger q = a / m;
+                BigInteger t = m;
+                m = a % m;
+                a = t;
+                t = y;
+                y = x - q * y;
+                x = t;
+            }
+
+            if (x < 0) x += m0;
+            return x;
+        }
+
+        // ===================== CRIPTARE =====================
+        private void btnEncrypt_Click(object sender, EventArgs ev)
+        {
+            try
+            {
+                if (e == 0 || n == 0)
+                {
+                    MessageBox.Show("Generează mai întâi cheile!");
+                    return;
+                }
+
+                byte[] data = Encoding.UTF8.GetBytes(txtPlain.Text);
+                int blockSize = (n.ToString().Length / 3) - 1;
+                List<string> blocks = new List<string>();
+
+                for (int i = 0; i < data.Length; i += blockSize)
+                {
+                    int len = Math.Min(blockSize, data.Length - i);
+                    byte[] block = new byte[len];
+                    Array.Copy(data, i, block, 0, len);
+                    BigInteger m = new BigInteger(block);
+                    if (m < 0) m = -m;
+                    BigInteger c = BigInteger.ModPow(m, e, n);
+                    blocks.Add(c.ToString());
+                }
+
+                txtEncrypted.Text = string.Join(" ", blocks);
+                MessageBox.Show("Mesaj criptat cu succes!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Eroare la criptare: " + ex.Message);
+            }
+        }
+
+        // ===================== DECRIPTARE =====================
+        private void btnDecrypt_Click(object sender, EventArgs ev)
+        {
+            try
+            {
+                if (d == 0 || n == 0)
+                {
+                    MessageBox.Show("Generează mai întâi cheile!");
+                    return;
+                }
+
+                string[] parts = txtEncrypted.Text.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                List<byte> bytes = new List<byte>();
+
+                foreach (string part in parts)
+                {
+                    BigInteger c = BigInteger.Parse(part);
+                    BigInteger m = BigInteger.ModPow(c, d, n);
+                    byte[] block = m.ToByteArray();
+                    bytes.AddRange(block);
+                }
+
+                txtDecrypted.Text = Encoding.UTF8.GetString(bytes.ToArray());
+                MessageBox.Show("Mesaj decriptat cu succes!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Eroare la decriptare: " + ex.Message);
+            }
+        }
+
+        // ===================== AFIȘARE CHEI =====================
+        private void btnP_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("P = " + p);
+        }
+
+        private void btnQ_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Q = " + q);
+        }
+
+        private void btnN_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("N = " + n);
+        }
+
+        private void btnPhi_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Φ(N) = " + phi);
+        }
+
+        private void btnE_Click(object sender, EventArgs ev)
+        {
+            MessageBox.Show("E = " + e);
+        }
+
+        private void btnD_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("D = " + d);
+        }
+    }
+}
